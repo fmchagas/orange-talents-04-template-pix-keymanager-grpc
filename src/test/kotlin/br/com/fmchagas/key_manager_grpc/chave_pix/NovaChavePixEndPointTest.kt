b8d2ca8eda134701272a80e159ee3ba98ad41ac6
Fernando Chagas <fermaccha@gmail.com>
2021-06-14 09:04:11 -0400

    Atualiza teste de integração do endpoint grpc cadastrar nova chave pix

--- src/test/kotlin/br/com/fmchagas/key_manager_grpc/chave_pix/NovaChavePixEndPointTest.kt
+++ src/test/kotlin/br/com/fmchagas/key_manager_grpc/chave_pix/NovaChavePixEndPointTest.kt
@@ -10,23 +10,44 @@ import io.grpc.StatusRuntimeException
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
-import java.util.*
+import io.micronaut.http.HttpResponse
+import io.micronaut.test.annotation.MockBean
 import javax.inject.Singleton
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Test
+import java.util.*
+import javax.inject.Inject
+import org.mockito.kotlin.*
 
 @MicronautTest(transactional = false)
 internal class NovaChavePixEndPointTest(
     val repository: ChavePixRepository,
     val clientGrpc: NovaChavePixServiceGrpc.NovaChavePixServiceBlockingStub
 ) {
+    @Inject
+    private lateinit var clientErpItau: InformacaoDasContasDoItauERPClient
+
+    /*@MockBean(InformacaoDasContasDoItauERPClient::class)
+    internal fun mockItauClient() = mock<InformacaoDasContasDoItauERPClient>()*/
+
+    @BeforeEach
+    internal fun setup() {
+        repository.deleteAll()
+    }
 
     @Test
     fun `deve registrar chave pix`() {
-        repository.deleteAll()
+
+        whenever(
+            clientErpItau.buscaViaHttp(
+                "5260263c-a3c1-4727-ae32-3bdb2538841b",
+                "POUPANCA"
+            )
+        ).thenReturn(HttpResponse.ok(informacaoDaContaResponseFake()))
 
         val response = clientGrpc.registrar(
             NovaChavePixRequest.newBuilder()
@@ -43,20 +64,20 @@ internal class NovaChavePixEndPointTest(
     }
 
     @Test
-    fun `deve registrar chave pix aleatoria`() {
-        repository.deleteAll()
+    fun `nao deve registrar chave pix quando parametos forem invalidos`() {
+        val error = assertThrows<StatusRuntimeException> {
+            clientGrpc.registrar(
+                NovaChavePixRequest.newBuilder()
+                    .setClienteId("invalido")
+                    .setTipoChave(TipoChave.CPF)
+                    .setChaveDoPix("")
+                    .setTipoConta(TipoConta.POUPANCA)
+                    .build()
+            )
+        }
 
-        val response = clientGrpc.registrar(
-            NovaChavePixRequest.newBuilder()
-                .setClienteId("5260263c-a3c1-4727-ae32-3bdb2538841b")
-                .setTipoChave(TipoChave.CHAVE_ALEATORIA)
-                .setChaveDoPix("")
-                .setTipoConta(TipoConta.POUPANCA)
-                .build()
-        )
-
-        with(response) {
-            assertNotNull(pixId)
+        with(error) {
+            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
         }
     }
 
@@ -86,6 +107,33 @@ internal class NovaChavePixEndPointTest(
             assertEquals("chave pix '${existente.chavePix}' já cadastrada no sistema", status.description)
         }
     }
+
+    @Test
+    fun `nao deve registrar chave pix quando conta do cliente nao for encontrada`() {
+        val error = assertThrows<StatusRuntimeException> {
+            clientGrpc.registrar(
+                NovaChavePixRequest.newBuilder()
+                    .setClienteId("ea691b01-4567-498b-83b7-1552df6cb1f4")
+                    .setTipoChave(TipoChave.CPF)
+                    .setChaveDoPix("73007268010")
+                    .setTipoConta(TipoConta.POUPANCA)
+                    .build()
+            )
+        }
+
+        with(error) {
+            assertEquals(Status.NOT_FOUND.code, status.code)
+            assertEquals("Conta do cliente não encontrada", status.description)
+        }
+    }
+
+    fun informacaoDaContaResponseFake() = InformacaoDaContaResponse(
+        tipo = "CONTA_POUPANCA",
+        instituicao = InstituicaoResponse(nome = "Banco X", ispb = "1023"),
+        agencia = "1010",
+        numero = "20202",
+        titular = TitularResponse(id = "5260263c-a3c1-4727-ae32-3bdb2538841b", nome = "Dunha", cpf = "73007268010")
+    )
 
     @Factory
     class Clientes {
